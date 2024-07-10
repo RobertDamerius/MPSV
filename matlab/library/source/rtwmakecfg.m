330d9dc51257599966ba2cebab87ccaa26c04a17
Robert Damerius <damerius.mail@gmail.com>
2024-07-11 00:53:48 +0200

    Revert to collision check of final trajectory to ensure dynamic feasibility.

--- matlab/library/source/rtwmakecfg.m
+++ matlab/library/source/rtwmakecfg.m
@@ -2,7 +2,7 @@ function makeInfo = rtwmakecfg()
 %RTWMAKECFG adds include and source directories to the make files.
 %    makeInfo=
 %    Simulink version    : 23.2 (R2023b) 01-Aug-2023
-%    MATLAB file generated on : 10-Jul-2024 21:41:22
+%    MATLAB file generated on : 11-Jul-2024 00:52:31
 
 % Verify the Simulink version
 verify_simulink_version();
